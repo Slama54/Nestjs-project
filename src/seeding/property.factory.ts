60f06fbd20eeaa978b01b717529e963887ac21b8
wael <wael.slama.issat@gmail.com>
2025-01-18 23:43:53 +0100

    seeding

---
+++ src/seeding/property.factory.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+
+
+import { Faker } from "@faker-js/faker";
+import { Property } from "../entities/property.entity";
+import { setSeederFactory } from "typeorm-extension";
+
+export const PropertyFactory = setSeederFactory(Property, (faker : Faker)=>{
+
+    const property = new Property();
+    property.name =faker.location.street()
+    property.price =+faker.commerce.price({min:10000,max:1000000});
+    property.description = faker.lorem.sentence();
+
+
+    return property;
+})
---
+++ src/seeding/user.factory.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {  Faker } from "@faker-js/faker";
+import { User } from "../entities/user.entity";
+import { setSeederFactory } from "typeorm-extension";
+
+export const UserFactory = setSeederFactory(User, (faker : Faker)=>{
+    const user = new User();
+    user.firstName =faker.person.firstName();
+    user.lastName = faker.person.lastName();
+    user.email = faker.internet.email();
+    user.avatarURl = faker.image.avatar();
+    return user;
+})
